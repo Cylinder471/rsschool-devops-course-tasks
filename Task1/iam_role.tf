111cb66c191a621f046f7eb238182ae1b77a9a7d
pavel.abalonik <pavel.abalonik@innowise.com>
2025-06-12 13:05:27 +0000

    iam

--- Task1/iam_role.tf
+++ Task1/iam_role.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_role" "github_actions_role" {
   name = "GithubActionsRole"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -11,9 +12,9 @@ resource "aws_iam_role" "github_actions_role" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
-          }
-          "StringLike" : {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+          },
+          "StringLike": {
             "token.actions.githubusercontent.com:sub" = "repo:Cylinder471/rsschool-devops-course-tasks:*"
           },
         }
@@ -22,8 +23,46 @@ resource "aws_iam_role" "github_actions_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "github_actions_policies" {
-  for_each   = toset(var.policies)
+resource "aws_iam_role_policy_attachment" "ec2_full_access" {
   role       = aws_iam_role.github_actions_role.name
-  policy_arn = each.value
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "route53_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
 }
