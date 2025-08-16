2224e1cac05bf01353b93b67dbf47a86718dd199
Pavel Abalonik <pavel.abalonik@innowise.com>
2025-08-16 16:44:21 +0200

    change iamrole

--- Task3/iamrole.tf
+++ Task3/iamrole.tf
@@ -14,30 +14,6 @@ resource "aws_iam_role" "ec2_role" {
     ]
   })
 }
-resource "aws_iam_role" "GithubActionsRole" {
-  name = "GithubActionsRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::650251696415:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:Cylinder471/rsschool-devops-course-tasks:*"
-          }
-        }
-      }
-    ]
-  })
-}
 resource "aws_iam_policy" "k3s_worker_ssm" {
   name = "${var.project_name}-k3s-worker-ssm-policy"
 
@@ -67,7 +43,9 @@ resource "aws_iam_policy" "k3s_worker_ssm" {
     ]
   })
 }
-
+data "aws_iam_role" "github_actions_role" {
+  name = "GithubActionsRole"
+}
 resource "aws_iam_role_policy_attachment" "k3s_worker_ssm_attach" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.k3s_worker_ssm.arn
@@ -102,6 +80,6 @@ resource "aws_iam_policy" "github_actions_ssm" {
 
 # Привязка политики к GitHub OIDC роли
 resource "aws_iam_role_policy_attachment" "github_actions_attach" {
-  role       = aws_iam_role.GithubActionsRole.name
+  role       = data.aws_iam_role.github_actions_role.name
   policy_arn = aws_iam_policy.github_actions_ssm.arn
 }
